00ab737583338b2017accef55b1463e3a2af451a
doteater <c0ry42@gmail.com>
2025-10-15 15:15:11 -0700

    fix init, ANOTHER old to new name fix

--- erpnext_calendar_tz_fix/__init__.py
+++ erpnext_calendar_tz_fix/__init__.py
@@ -1,13 +1,4 @@
+# erpnext_calendar_tz_fix/__init__.py
+"""ERPNext Calendar Timezone Fix - Fixes timezone issues in calendar events"""
+
 __version__ = "0.0.1"
-
-def _apply_patch():
-    """Apply the monkey patch when explicitly called, not at import time"""
-    try:
-        from calendar_fix.patches.calendar_timezone_fix import apply_monkey_patch
-        apply_monkey_patch()
-    except ImportError:
-        import frappe
-        # rest of the patch logic
-
-# Don't call _apply_patch() here at module level
-# It should be called by frappe's hooks or explicitly when needed
